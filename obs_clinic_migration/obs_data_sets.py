dbf0548e1d39c9fd359cd01641efff0bcc536528
rseeto <ryan.seeto@alum.utoronto.ca>
2022-08-03 13:13:28 -0400

    Minor style changes to obs_data_sets

--- obs_clinic_migration/obs_data_sets.py
+++ obs_clinic_migration/obs_data_sets.py
@@ -5,20 +5,20 @@ import pandas as pd
 
 rave_clinic = pd.read_csv(
     Path(__file__).parent/"../data/raw/OBS Flat Output 09SEP2019_475.csv",
-    encoding = 'mbcs',
-    low_memory = False,
-    dtype = str
+    encoding='mbcs',
+    low_memory=False,
+    dtype=str
 )
 
 redcap_data_dict = pd.read_csv(
     Path(__file__).parent/"../data/raw/OBSUAT_DataDictionary_2020-05-13.csv",
-    low_memory = False,
-    dtype = str
+    low_memory=False,
+    dtype=str
 )
 
 redcap_clinic = pd.read_csv(
     Path(__file__).parent/"../data/raw/redcap_double_data_entry.csv",
-    encoding = 'mbcs',
-    low_memory = False,
-    dtype = str
+    encoding='mbcs',
+    low_memory=False,
+    dtype=str
 )
